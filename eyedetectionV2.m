b32864685653a2d5fd6d7c3043df4b2ac78626a6
IvarGorenko <73782886+IvarGorenko@users.noreply.github.com>
2022-12-07 18:35:16 +0100

    Eyedetection whitebalance fix

--- eyedetectionV2.m
+++ eyedetectionV2.m
@@ -1,6 +1,9 @@
 function [eye1, eye2] = eyedetectionV2(im)
 % EYEDETECTIONV1 takes image input and returns two 1x2 vectors, one for each eyes x,y position.
 
+% vitbalans, endast för bilder med tonförändring
+im = AWB_max(im);
+
 % applicera facemask
 facemask = skinDetection(im);
 im = im.*facemask;
--- test_eyedetection.m
+++ test_eyedetection.m
@@ -1,7 +1,7 @@
 clear
-im = imread("images/DB2/cl_04.jpg");
+im = imread("images/DB3/db1_06.jpg");
 im = im2double(im); % måste konvertera den till double innan vi skickar till facemask, för eyedetect gör det ingen skillnad
-imshow(im);
+%imshow(im);
 [eye1, eye2] = eyedetectionV2(im);
 disp(eye1);
 disp(eye2);
