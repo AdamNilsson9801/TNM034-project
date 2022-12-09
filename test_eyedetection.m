78cf139842b14047d7e1dd4a9aaab5ef4559d7bd
IvarGorenko <73782886+IvarGorenko@users.noreply.github.com>
2022-12-09 15:37:34 +0100

    eyedetection testing can be done automatically

--- test_eyedetection.m
+++ test_eyedetection.m
@@ -1,8 +1,95 @@
 clear
-im = imread("images/DB3/db1_06.jpg");
-im = im2double(im); % måste konvertera den till double innan vi skickar till facemask, för eyedetect gör det ingen skillnad
-%imshow(im);
-[eye1, eye2] = eyedetectionV2(im);
-disp(eye1);
-disp(eye2);
+num_success = 0;
+num_error = 0;
 
+% Test DB1
+disp(" ");
+disp("DB1")
+for n=1:16
+    im{n} = imread(sprintf('images/DB1/db1_%02d.jpg',n));
+    im{n} = im2double(im{n}); % måste konvertera den till double innan vi skickar 
+    [eye1, eye2] = eyedetectionV2(im{n});
+    if eye1(1) > 0 && eye2(1) > 0
+        disp("image db1_" + n + ": SUCCESS");
+        num_success = num_success + 1;
+    else
+        disp("image db1_" + n + ": ERROR");
+        num_error = num_error + 1;
+    end
+end
+
+disp(" ");
+disp("DB2");
+for n=[1,2,4,5,6,7,10,13,14]
+    im{n} = imread(sprintf('images/DB2/bl_%02d.jpg',n));
+    im{n} = im2double(im{n}); 
+    [eye1, eye2] = eyedetectionV2(im{n});
+    if eye1(1) > 0 && eye2(1) > 0
+        disp("image bl_" + n + ": SUCCESS");
+        num_success = num_success + 1;
+    else
+        disp("image bl_" + n + ": ERROR");
+        num_error = num_error + 1;
+    end
+end
+
+disp(" ");
+for n= 1:16
+    im{n} = imread(sprintf('images/DB2/cl_%02d.jpg',n));
+    im{n} = im2double(im{n}); 
+    [eye1, eye2] = eyedetectionV2(im{n});
+    if eye1(1) > 0 && eye2(1) > 0
+        disp("image cl_" + n + ": SUCCESS");
+        num_success = num_success + 1;
+    else
+        disp("image cl_" + n + ": ERROR");
+        num_error = num_error + 1;
+    end
+end
+
+disp(" ");
+for n= [1,3,4,7,9,11,12]
+    im{n} = imread(sprintf('images/DB2/ex_%02d.jpg',n));
+    im{n} = im2double(im{n}); 
+    [eye1, eye2] = eyedetectionV2(im{n});
+    if eye1(1) > 0 && eye2(1) > 0
+        disp("image ex_" + n + ": SUCCESS");
+        num_success = num_success + 1;
+    else
+        disp("image ex_" + n + ": ERROR");
+        num_error = num_error + 1;
+    end
+end
+
+disp(" ");
+for n= [1,7,8,9,12,16]
+    im{n} = imread(sprintf('images/DB2/il_%02d.jpg',n));
+    im{n} = im2double(im{n}); 
+    [eye1, eye2] = eyedetectionV2(im{n});
+    if eye1(1) > 0 && eye2(1) > 0
+        disp("image il_" + n + ": SUCCESS");
+        num_success = num_success + 1;
+    else
+        disp("image il_" + n + ": ERROR");
+        num_error = num_error + 1;
+    end
+end
+
+disp(" ");
+disp("DB3")
+for n=1:16
+    im{n} = imread(sprintf('images/DB1/db1_%02d.jpg',n));
+    im{n} = im2double(im{n});
+    [eye1, eye2] = eyedetectionV2(im{n});
+    if eye1(1) > 0 && eye2(1) > 0
+        disp("image db1_" + n + ": SUCCESS");
+        num_success = num_success + 1;
+    else
+        disp("image db1_" + n + ": ERROR");
+        num_error = num_error + 1;
+    end
+end
+
+disp(" ");
+disp(" Successes: " + num_success);
+disp(" Errors: " + num_error);
