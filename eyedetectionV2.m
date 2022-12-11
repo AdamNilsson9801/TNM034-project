97daa03dcd66a21eed8210d1ab1088ac12047bc5
IvarGorenko <73782886+IvarGorenko@users.noreply.github.com>
2022-12-11 20:48:44 +0100

    eyedetection fix

--- eyedetectionV2.m
+++ eyedetectionV2.m
@@ -9,10 +9,10 @@ facemask = skinDetection(im);
 im = im.*facemask;
 
 % munposition, om mouth_pos (0,0) hittades den inte
-[~, mouth_pos] = createMouthMap(im);
+[~, mouth_pos] = mouthDetection(im);
 
 % konvertera bilden till NTSC format för att enklare hitta eye dot genom chromasitet 
-% använd endast chromasity kanalen
+% använd endast luminocity kanalen
 YIQim = rgb2ntsc(im);
 imChrom = YIQim(:,:,1);
 
@@ -32,12 +32,9 @@ thresh = 0.8; step = 0.05;
 blob_minarea = round(im_area/10000); blobs_maxarea = round(im_area/1000);
 counter_minarea = 0; counter_maxarea = 0;
 imThresh = imDiff(:,:) > thresh;
-blobs = regionprops(imThresh);
-blobs_amount = length(blobs(:)); % det här värdet används alltid
 
 while counter_minarea < 7 && counter_maxarea < 2 && thresh > 0.35
     counter_minarea = 0; counter_maxarea = 0;
-    thresh = thresh - step;
     imThresh = imDiff(:,:) > thresh;
     blobs = regionprops(imThresh);
     blobs_amount = length(blobs(:));
@@ -49,6 +46,7 @@ while counter_minarea < 7 && counter_maxarea < 2 && thresh > 0.35
             end
         end
     end
+        thresh = thresh - step;
 end
 
 % Gör ögon tydligare, ta bort noise genom morfologisk stängning
@@ -75,43 +73,17 @@ if blobs_amount > 2
     end
 end
 
-% Testar varje blobb mot varandra. Om längre än 1/1 av bild +- samt +-15 grader, ta bort
-eye_dist = round(im_dist/3.5); eye_tol = round(eye_dist/1);
-blobs = regionprops(imThresh);
-blobs_amount = length(blobs(:));
-angle_lim = 0.26; % ca 15 grader
-blob_current = 1;
-while blob_current < blobs_amount + 1 && blobs_amount > 2
-    blob_candidate = false;
-    for i = 1:blobs_amount
-        % sqrt((x1-x2)^2+(y1-y2)^2)
-        % asin(motstående katet/hypotenusa) = vinkel i radianer
-        hypotenuse = sqrt((blobs(blob_current).Centroid(1) - blobs(i).Centroid(1))^2 + (blobs(blob_current).Centroid(2) - blobs(i).Centroid(2))^2);
-        perpendicular = abs(blobs(blob_current).Centroid(2) - blobs(i).Centroid(2));
-        angle = asin(perpendicular/hypotenuse);
-        if i ~= blob_current && abs(eye_dist - hypotenuse) < eye_tol && angle < angle_lim
-            blob_candidate = true;
-        end
-    end
-    if blob_candidate == false
-        box = blobs(blob_current).BoundingBox;
-        imThresh(round(box(2)):round(box(4) + box(2)-1), round(box(1)):round(box(3) + box(1)-1)) = 0;
-    end
-    blob_current = blob_current + 1;
-end
-
-% ta ögon enligt munnens position (kollar distans och vinkel)
+% ta ögon enligt munnens position (kollar vinkel)
 if mouth_pos(1) > 0 && mouth_pos(2) > 0
-    mouth_dist = round(im_dist/2.8);  mouth_tol = round(mouth_dist/1);
     blobs = regionprops(imThresh);
     blobs_amount = length(blobs(:));
-    angle_upper = 1.570796; % 90 grader
+    angle_upper = 1.396263; % 80 grader
     angle_lower = 0.7853982; % 45 grader
     for blob_current = 1:1:(blobs_amount)
         hypotenuse = sqrt((blobs(blob_current).Centroid(1) - mouth_pos(1))^2 + (blobs(blob_current).Centroid(2) - mouth_pos(2))^2);
         perpendicular = abs(blobs(blob_current).Centroid(2) - mouth_pos(2));
         angle = asin(perpendicular/hypotenuse);
-        if abs(mouth_dist - hypotenuse) > mouth_tol || angle > angle_upper || angle < angle_lower
+        if  angle > angle_upper || angle < angle_lower
             box = blobs(blob_current).BoundingBox;
             imThresh(round(box(2)):round(box(4) + box(2)-1), round(box(1)):round(box(3) + box(1)-1)) = 0;
         end
@@ -153,7 +125,6 @@ if eyes_amount == 2
 else
     eye1 = zeros(1,2);
     eye2 = zeros(1,2);
-    disp("ERROR: Could not find the eyes");
-    imshow(imThresh);
+    % disp("ERROR: Could not find the eyes");
 end
 end
--- test_eyedetection.m
+++ test_eyedetection.m
@@ -9,6 +9,9 @@ for n=1:16
     im{n} = imread(sprintf('images/DB1/db1_%02d.jpg',n));
     im{n} = im2double(im{n}); % måste konvertera den till double innan vi skickar 
     [eye1, eye2] = eyedetectionV2(im{n});
+    pos = [eye1; eye2];
+    showim = insertMarker(im{n}, pos, 'star','color','red','size',10);
+    imshow(showim);
     if eye1(1) > 0 && eye2(1) > 0
         disp("image db1_" + n + ": SUCCESS");
         num_success = num_success + 1;
@@ -24,6 +27,9 @@ for n=[1,2,4,5,6,7,10,13,14]
     im{n} = imread(sprintf('images/DB2/bl_%02d.jpg',n));
     im{n} = im2double(im{n}); 
     [eye1, eye2] = eyedetectionV2(im{n});
+    pos = [eye1; eye2];
+    showim = insertMarker(im{n}, pos, 'star','color','red','size',10);
+    imshow(showim);
     if eye1(1) > 0 && eye2(1) > 0
         disp("image bl_" + n + ": SUCCESS");
         num_success = num_success + 1;
@@ -38,6 +44,9 @@ for n= 1:16
     im{n} = imread(sprintf('images/DB2/cl_%02d.jpg',n));
     im{n} = im2double(im{n}); 
     [eye1, eye2] = eyedetectionV2(im{n});
+    pos = [eye1; eye2];
+    showim = insertMarker(im{n}, pos, 'star','color','red','size',10);
+    imshow(showim);
     if eye1(1) > 0 && eye2(1) > 0
         disp("image cl_" + n + ": SUCCESS");
         num_success = num_success + 1;
@@ -52,6 +61,9 @@ for n= [1,3,4,7,9,11,12]
     im{n} = imread(sprintf('images/DB2/ex_%02d.jpg',n));
     im{n} = im2double(im{n}); 
     [eye1, eye2] = eyedetectionV2(im{n});
+    pos = [eye1; eye2];
+    showim = insertMarker(im{n}, pos, 'star','color','red','size',10);
+    imshow(showim);
     if eye1(1) > 0 && eye2(1) > 0
         disp("image ex_" + n + ": SUCCESS");
         num_success = num_success + 1;
@@ -66,6 +78,9 @@ for n= [1,7,8,9,12,16]
     im{n} = imread(sprintf('images/DB2/il_%02d.jpg',n));
     im{n} = im2double(im{n}); 
     [eye1, eye2] = eyedetectionV2(im{n});
+    pos = [eye1; eye2];
+    showim = insertMarker(im{n}, pos, 'star','color','red','size',10);
+    imshow(showim);
     if eye1(1) > 0 && eye2(1) > 0
         disp("image il_" + n + ": SUCCESS");
         num_success = num_success + 1;
@@ -78,9 +93,12 @@ end
 disp(" ");
 disp("DB3")
 for n=1:16
-    im{n} = imread(sprintf('images/DB1/db1_%02d.jpg',n));
+    im{n} = imread(sprintf('images/DB3/db1_%02d.jpg',n));
     im{n} = im2double(im{n});
     [eye1, eye2] = eyedetectionV2(im{n});
+    pos = [eye1; eye2];
+    showim = insertMarker(im{n}, pos, 'star','color','red','size',10);
+    imshow(showim);
     if eye1(1) > 0 && eye2(1) > 0
         disp("image db1_" + n + ": SUCCESS");
         num_success = num_success + 1;
