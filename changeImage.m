a5bdbbae124924ebfef7c8aaba5b9e22671e5f4e
Adamski9801 <nilssonadam9801@gmail.com>
2022-12-13 11:20:49 +0100

    .

--- changeImage.m
+++ changeImage.m
@@ -4,6 +4,7 @@ function im = changeImage(im)
 toneMin = 0.7;
 toneMax = 1.3;
 randTone = toneMin + rand*(toneMax - toneMin);
+disp('-------------');disp('random tone:');disp(randTone);
 
 %Tone the image
 im = im.*randTone;
@@ -12,6 +13,7 @@ im = im.*randTone;
 scaleMin = 0.9;
 scaleMax = 1.1;
 randScale = scaleMin + rand*(scaleMax - scaleMin);
+disp('random scale:');disp(randScale);
 
 %Scale the image
 im = im.*randScale;
@@ -20,6 +22,7 @@ im = im.*randScale;
 rotationMin = -5;
 rotationMax = 5;
 randRotation = rotationMin + rand*(rotationMax - rotationMin);
+disp('random rotation:');disp(randRotation);disp('-------------');
 
 %Rotate the image
 im = imrotate(im, randRotation, 'bilinear'); 
